f0ded0a1e554110e97f02f43a2c0596bd1f6b715
dihlare <Djsethosa@gmail.com>
2023-09-07 14:34:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
-"Programming is like building a multilingual puzzle
+int main() {
+    puts("Programming is like building a multilingual puzzle");
