e8cda431e813b8611b6a5650520df8b10c223cd0
dihlare <Djsethosa@gmail.com>
2023-09-08 00:21:12 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - This is the main function
  *
- *Return: always 0
+ * Return: Always 0
  */
-
-int main() {
+int main(void)
+{
     puts("Programming is like building a multilingual puzzle");
     return (0);
 }
+
