c4050a1b772bed549c4057bea6b360e54e98e3fd
dihlare <Djsethosa@gmail.com>
2023-09-13 11:58:21 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,11 @@ int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('u');              
-_putchar('t');              
+_putchar('u');
+_putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('\n');
-return (0);     
+return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,16 @@
 void print_alphabet(void)
 
 {
-
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
 
 {
 
-	_putchar(c);
-}
-_putchar('\n');
+_putchar(c);
+
 }
 
-	
+_putchar('\n');
+
+}
